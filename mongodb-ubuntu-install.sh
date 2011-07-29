367ec063b9542601b6e85f01eda14dc36f9b4790
Dan Simmons <xiode04@gmail.com>
2011-07-29 20:39:34 +0000

    New install script for latest MongoDB from 10gen

---
+++ mongodb-ubuntu-install.sh
@@ -0,0 +1,3 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo sh -c 'echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list'
+sudo apt-get upgrade && sudo apt-get install -y mongodb-10gen
